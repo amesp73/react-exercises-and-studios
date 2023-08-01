62a24a6ff732db9d7835b88f02281f15425c9025
amesp73 <amitrpatel502@gmail.com>
2023-07-31 20:12:14 -0500

    edited part 1

--- part1/exercises/src/components/ChoresList.js
+++ part1/exercises/src/components/ChoresList.js
@@ -1,3 +1,11 @@
 export default function ChoresList () {
-   return <p>No content is here!</p>;
+   return(
+      <div>
+          <h3 className = {classes.choresHeading}>Today's Chores</h3>
+          <ul>
+            <li className={classes.choresText}>Vaccuum</li>
+            <li className={class.choresText}>Sweep</li>
+          </ul>
+      </div>
+   );
 }
