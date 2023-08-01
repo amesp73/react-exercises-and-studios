760e245063ee98ab983c32626ef3d11fa72f84da
amesp73 <amitrpatel502@gmail.com>
2023-07-31 19:49:50 -0500

    updated movie list, movie css, and book list

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "My Favorite Books";
+   let book1 = "https://upload.wikimedia.org/wikipedia/en/d/df/The_Silent_Patient_early_2019_UK_edition.png";
+   let book2 = "https://www.publishersweekly.com/cover/9781250133731";
+   let book3 = "https://upload.wikimedia.org/wikipedia/en/5/56/TheNameoftheWind_cover.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="The Silent Patient" />
+         <img src={book2} alt="An Anonymous Girl" />
+         <img src={book3} alt="Name of the Wind" />
       </div>      
    );
 }
