39a33977d3d93cda06af4ba1f9be58ec1916bfd2
amesp73 <amitrpatel502@gmail.com>
2023-08-01 21:46:06 -0500

    completed remainder of part 3 REACT PART 2 COMPLETE

--- part2/exercises-2/src/components/Profile.js
+++ part2/exercises-2/src/components/Profile.js
@@ -3,7 +3,15 @@ import Button from './Button';
 import oceans from './oceans.json';
 
 const listItem = oceans.map(ocean => 
-   <div key={ocean.id}>
+   <div className = {`${ocean.fishCheck === "true" ? "isAFish" : "profile"}`} key={ocean.id}>
+      <h1>{ocean.name}</h1>
+      <h2>Fun Facts</h2>
+         <ul>
+            <li>{ocean.fact1}</li>
+            <li>{ocean.fact2}</li>
+            <li>{ocean.fact3}</li>
+         </ul>
+         <Button />
       <img className="img" src={ocean.image} alt={ocean.name}/>
    </div>
    )
@@ -12,12 +20,9 @@ const listItem = oceans.map(ocean =>
 function Profile()
 {
    return(
-      <>
-         <ul>
-            {listItem}
-         </ul>
-         <Button />
-      </>  
+      <ul>
+         {listItem}
+      </ul>
    );
 }
 
