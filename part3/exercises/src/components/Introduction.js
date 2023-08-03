d5eb1d3851716ebce123df05487fe744ac275a0d
amesp73 <amitrpatel502@gmail.com>
2023-08-02 20:40:57 -0500

    completed part 3 exercises

---
+++ part3/exercises/src/components/Introduction.js
@@ -0,0 +1,13 @@
+export default function HobbyIntroduction() {
+    return(
+        <div>
+            <h2>3 Fun Facts About this Hobby</h2>
+            <ol>
+                <li>104.3 calories are burned every mile when running at a 10 minute mile pace</li>
+                <li>The fastest mile was in 3:43:13 in 1999</li>
+                <li>Regular running is good for improving long term stability of bones</li>
+            </ol>
+        </div>
+    );
+}
+
---
+++ part3/exercises/src/components/Projects.js
@@ -0,0 +1,27 @@
+import data from '../data.json';
+import {useState} from 'react';
+
+
+export default function MyProjects() {
+    const [index, setIndex] = useState(0);
+
+    let projects = data.projects;
+    let project = projects[index];
+
+    function handleClick() {
+        if (index < projects.length - 1) {
+            setIndex(index + 1);
+        } else {
+            setIndex(0);
+        }
+    }
+    return(
+        <div>
+            <button onClick = {handleClick}>
+                Next
+            </button>
+            <h2>{project.location} in {project.season}</h2>
+            <img src = {project.photoUrl} alt = {project.alt} />
+        </div>
+    );
+}
